57417f98945be7e82df412a5012e7c4adb6a892e
hadeer <109919062+HadeerMou@users.noreply.github.com>
2025-01-01 23:04:58 +0200

    upload

---
+++ contact/contact.js
@@ -0,0 +1,8 @@
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let inquiryType = document.getElementById("inquiryType");
+let message = document.getElementById("message");
+
+if (name.value == "") {
+  name.value === "the name is required";
+}
